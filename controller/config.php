1035b4c4d8d8a213da845682af3c9f10bfb3bcf5
Bayu Wicaksono <bayuw1123@gmail.com>
2024-12-17 00:05:02 +0700

    fix mysql config

--- controller/config.php
+++ controller/config.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
+$host = "db";
+$user = "lestari";
+$password = "password";
 $dbname = "db_sampah_4";
 
 $conn = new mysqli($host, $user, $password, $dbname);
